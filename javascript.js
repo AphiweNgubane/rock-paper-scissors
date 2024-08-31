783c8178ca07b7eb1f52c1e1638f638c768416ce
AphiweNgubane <164533620+AphiweNgubane@users.noreply.github.com>
2024-08-31 22:08:46 +0200

    Add restriction that user inputs valid choice to proceed

--- javascript.js
+++ javascript.js
@@ -10,10 +10,14 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
 
+let userInput= window.prompt("Pick one: Rock, Paper, or Scissors")
 
-const chooseOption= window.prompt("Pick one: Rock, Paper, or Scissors")
 function getHumanChoice() {
-    return chooseOption;
+  if (choices.includes(userInput)) {
+    return userInput
+  } else  {
+    alert("Invalid choice");
+    window.prompt("Please only choose one between: Rock, Paper, Scissors");
+  }
 }
-
-console.log(chooseOption)
+console.log(getHumanChoice())
